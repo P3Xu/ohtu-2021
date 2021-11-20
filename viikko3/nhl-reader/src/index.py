5ca2f749a826c40aeca26ba474095a913ee92ab2
Pekka <75335748+P3Xu@users.noreply.github.com>
2021-11-20 13:54:10 +0200

    Assignment 1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,25 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        if player_dict['nationality'] == "FIN":
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists']
+            )
+
+            players.append(player)
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
